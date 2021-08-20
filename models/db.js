5036780a4bf91abac40f9f0bae2b7a61251879c9
Riya Vijay Vishwakarma <s1032190258@thakureducation.org>
2021-08-20 20:10:02 +0530

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,8 +1,6 @@
 const mongoose = require("mongoose");
 
-conn_str =
-  "mongodb://jobp_user:jobp123@cluster0-shard-00-00.xghz9.mongodb.net:27017,cluster0-shard-00-01.xghz9.mongodb.net:27017,cluster0-shard-00-02.xghz9.mongodb.net:27017/job-db?ssl=true&replicaSet=atlas-ekl5ih-shard-0&authSource=admin&retryWrites=true&w=majority";
-
+conn_str =//add your database's connection string
 mongoose.connect(
   conn_str,
   { userNewUrlParser: true, useUnifiedTopology: true },
